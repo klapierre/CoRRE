5eff4fa33d78b2100c3dfc2990eb59cc68ec9a8d
Meghan Avolio <meghan.avolio@gmail.com>
2024-01-12 16:29:54 -0500

    but update to BGP see notes in code

--- DatasetCleaningCode/KNZ_BGP.R
+++ DatasetCleaningCode/KNZ_BGP.R
@@ -3,6 +3,14 @@
 ################
 #setwd("~/Dropbox/CoRRE_database")
 
+library(tidyverse)
+
+####notes: We stopped mowing after 2003 because most of the plots that were mowed were heavily invaded by old worlds bluestem. If you are wanting to use data from across the entire 1989-2019 period, I would drop all of the mowed plots. Even before we stopped mowing, they were impacted by invasion to various degrees and ANPP sampling would have had to have been adjusted to account for that. I’m sure some of the species composition plots were impacted by invasion. 
+
+#The nutrient treatments continued uninterrupted through 2016. Fertilization ceased in 2017 
+
+#We are going only keep through 2016 the last year of fertilization and dropping all the mowing treatment
+
 # biomass data
 file <- "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-knz.57.10&entityid=eaf1b05d4c7578a1fe1efc5173b80953"
 df <- read.csv(file, header = TRUE, na.strings = c(NA, ""))
@@ -21,6 +29,7 @@ for (i in 1:nrow(df)){
 
 # combine treatment data into single column
 df$treatment <- paste(df$BURN, df$MOW, df$NUTRIENT, sep = "_")
+
 df <- df[df$MOW == "u",]
 
 df <- df[-which(is.na(df$FORBS) & is.na(df$LVGRASS)),]
@@ -38,14 +47,17 @@ df$anpp <- df$anpp*10 # mulitply by 10 to get g/m2, currently g/0.1 m2
 
 bgp.anpp <- df[,c(1,3,2,7,8,5,6,4)]
 
-#write.csv(df, "C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data\\CleanedData\\Sites\\ANPP csv\\KNZ_BGP_anpp.csv", row.names = FALSE)
+bgp.anpp2<-bgp.anpp %>% 
+  filter(calendar_year<2017) 
+
+#write.csv(df, "C:\\Users\\mavolio2\\Dropbox\\CoRRE_database\\Data\\CleanedData\\Sites\\ANPP csv\\KNZ_BGP_anpp.csv", row.names = FALSE)
 
 
 file1 <- "https://pasta.lternet.edu/package/data/eml/knb-lter-knz/17/11/410e032a0651ce990c8c497be62c68f7"
 df1 <- read.csv(file1, header = TRUE)
-file2 <- "http://lter.konza.ksu.edu/file/1607/download"
+file2 <- "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-knz.134.3&entityid=4abeadee64638e46bd5088f2fa7d832e"
 spdf <- read.csv(file2, header = TRUE)
-trtdf <- read.csv("C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data\\OriginalData\\Sites\\KNZ_BGP\\BGP_treatments.csv")
+trtdf <- read.csv("C:\\Users\\mavolio2\\Dropbox\\CoRRE_database\\Data\\OriginalData\\Sites\\KNZ_BGP\\BGP_treatments.csv")
 colnames(trtdf)[1] <- "plot_id"
 
 ## Convert cover class to mid-points
@@ -76,9 +88,16 @@ df1$data_type <- "cover"
 
 # need to add treatments from biomass data to cover data
 df2 <- merge(df1, trtdf, by = "plot_id", all.x = TRUE)
+df2 <- df2[df2$mowed == "u",]
 df2$treatment <- paste(df2$burned, df2$mowed, df2$nutrient, sep = "_")
 
-bgp.species <- df2[,c(1,4:8,17,18,22)]
+bgp.species <- df2[,c(1,4:8,19,23)]
 
-#write.csv(bgp.species, "C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data\\CleanedData\\Sites\\Species csv\\KNZ_BGP.csv", row.names = FALSE)
+bgp.species2<-bgp.species %>% 
+  filter(calendar_year<2017) %>% 
+  mutate(drop=ifelse(treatment %in% c('u_m_n', 'u_m_p', 'u_m_b', 'u_m_c','b_m_n', 'b_m_p', 'b_m_b', 'b_m_c'), 1, 0)) %>% 
+  filter(drop!=1) %>% 
+  select(-drop)
+
+#write.csv(bgp.species, "C:\\Users\\mavolio2\\Dropbox\\CoRRE_database\\Data\\CleanedData\\Sites\\Species csv\\KNZ_BGP.csv", row.names = FALSE)
 
